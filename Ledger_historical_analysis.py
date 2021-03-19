e13593bca51b013fc67287cf0fad0ccac8003fda
ZeNoonan <darragh.noonan@gmail.com>
2021-03-19 12:56:10 +0000

    update

--- Ledger_historical_analysis.py
+++ Ledger_historical_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import datetime
 
 # from Ledger_helper_functions import*
 from Ledger_helper_functions import (Budget_Raw_Clean_File,NL_Raw_Clean_File, date_selection_for_PL, PL_generation, merge_pl_dataframe,clean_format_PL_presentation,
@@ -16,6 +17,8 @@ Project_codes=pd.read_excel('C:/Users/Darragh/Documents/Python/Work/Data/Project
 data_2021='C:/Users/Darragh/Documents/Python/Work/Data/NL_2021_06.xlsx'
 data_2020='C:/Users/Darragh/Documents/Python/Work/Data/NL_2020.xlsx'
 data_2016_20='C:/Users/Darragh/Documents/Python/Work/Data/NL_2016_2019.xlsx'
+forecast_resourcing_file='C:/Users/Darragh/Documents/Python/Work/Data/Resource_Planner_v0005_2021-03-18 11_14_58.xlsx'
+forecast_project_mapping=pd.read_excel('C:/Users/Darragh/Documents/Python/Work/Data/Project_Codes_2021_.xlsx', sheet_name='Sheet2')
 
 @st.cache
 def load_ledger_data(data_2021):
@@ -51,3 +54,28 @@ st.write(format_gp(create_pivot_comparing_production_headcount(pivot_headcount(f
 # # https://stackoverflow.com/questions/49795825/skip-nan-and-shift-elements-in-a-pandas-dataframe-row  
 
 st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+forecast_resourcing=load_ledger_data(forecast_resourcing_file).copy()
+forecast_resourcing=(pd.merge(forecast_resourcing,forecast_project_mapping,on='Project',how='outer')).drop('Project',axis=1).rename(columns={'Project_name':'Project'})
+st.write(forecast_resourcing.head())
+st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+# st.write(pd.wide_to_long(forecast_resourcing,i='Project'))
+# st.write(forecast_resourcing.set_index('Project').unstack(level='Project').reset_index())
+# st.write(forecast_resourcing.set_index('Project').unstack())
+st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+
+forecast_resourcing.columns= forecast_resourcing.columns.astype(str)
+# st.write(forecast_resourcing.columns)
+# st.write(forecast_resourcing.loc[:,['Title','Total Months']])
+# st.write(forecast_resourcing.loc[:,['2021-03-01 00:00:00']])
+sliced_headcount_df=(forecast_resourcing.loc[:,'2021-03-01 00:00:00':])
+sliced_headcount=sliced_headcount_df.set_index('Project').unstack(level='Project').reset_index().rename(columns={'level_0':'date',0:'headcount'})
+st.write(sliced_headcount)
+
+st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+# Project_col=forecast_resourcing.iloc[:,-1:] # to select Project
+# months_headcount=forecast_resourcing.iloc[:,25:-1] # to select from march '21 to 2nd last column as last column has Project column
+# st.write(months_headcount)
+# df=pd.concat([Project_col, months_headcount],axis=1)
+# st.write(df)
+# st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+# st.write(forecast_resourcing['Project'].unique())
