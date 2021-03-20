b5b38010e2fb8e6811f26a6aabd2e7ea3565d1fa
ZeNoonan <darragh.noonan@gmail.com>
2021-03-20 20:45:18 +0000

    update

--- Ledger_historical_analysis.py
+++ Ledger_historical_analysis.py
@@ -27,51 +27,74 @@ def load_ledger_data(data_2021):
 NL = load_ledger_data(data_2021).copy()
 coding_acc_schedule = (pd.read_excel('C:/Users/Darragh/Documents/Python/Work/Data/account_numbers.xlsx')).iloc[:,:3]
 coding_sort=pd.read_excel('C:/Users/Darragh/Documents/Python/Work/Data/account_numbers.xlsx', sheet_name='Sheet2')
-NL_Data = NL_Raw_Clean_File(NL, coding_acc_schedule)
+
+def month_period_clean(x):
+    x['calendar_month']=x['Per.'].map({1:9,2:10,3:11,4:12,5:1,6:2,7:3,8:4,9:5,10:6,11:7,12:8,19:8})
+    x['calendar_year'] = np.where((x['Per.'] > 4.1), x['Yr.'], x['Yr.']-1)
+    return x
+
+
+
+
+NL_Data = month_period_clean(NL_Raw_Clean_File(NL, coding_acc_schedule)).copy()
+# st.write('testing calendar dates', NL_Data.head())
+
 
 NL_16_20 = load_ledger_data(data_2016_20).copy()
 NL_Data_16_20 = NL_Raw_Clean_File(NL_16_20, coding_acc_schedule)
 NL_Data_16_20=NL_Data_16_20.join(NL_Data_16_20['Employee'].str.split(' ', expand=True).rename(columns={0:'EE',1:'Name_EE'}))
 NL_Data_16_20['Employee - Ext. Code']=NL_Data_16_20['EE']
+NL_Data_16_20=month_period_clean(NL_Data_16_20)
 
 NL_20 = load_ledger_data(data_2020).copy()
-NL_Data_20 = NL_Raw_Clean_File(NL_20, coding_acc_schedule)
+NL_Data_20 = month_period_clean(NL_Raw_Clean_File(NL_20, coding_acc_schedule))
+
 
 @st.cache
 def df_concat():
     return pd.concat([NL_Data_16_20,NL_Data_20,NL_Data],ignore_index=True)
+# BUT I HAVE THE PROBLEM OF YEAR for example, period 3 which is equal to November in year 2016 will be made to Nov '16 when it should be Nov '15
+
 
 consol_headcount_data=df_concat().copy()
 headcount_filtered=final_headcount(consol_headcount_data).groupby('date').head(2)
-pivot_headcount_summary=format_gp(pivot_headcount(final_headcount(consol_headcount_data)))
+pivot_headcount_summary=(pivot_headcount(final_headcount(consol_headcount_data)))
 st.write('headcount filtered by top 2 in every month')
 st.write(headcount_filtered)
 st.write('pivot by month of headcount')
-st.write(pivot_headcount_summary)
+st.write(format_gp(pivot_headcount_summary))
 st.write('data to plot headcount numbers')
-st.write(format_gp(create_pivot_comparing_production_headcount(pivot_headcount(final_headcount(consol_headcount_data)))))
+st.write(format_gp(create_pivot_comparing_production_headcount(pivot_headcount_summary)))
 # https://stackoverflow.com/questions/14745022/how-to-split-a-dataframe-string-column-into-two-columns
 # # https://stackoverflow.com/questions/49795825/skip-nan-and-shift-elements-in-a-pandas-dataframe-row  
 
 st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+st.write('this is forecast headcount section')
 forecast_resourcing=load_ledger_data(forecast_resourcing_file).copy()
 forecast_resourcing=(pd.merge(forecast_resourcing,forecast_project_mapping,on='Project',how='outer')).drop('Project',axis=1).rename(columns={'Project_name':'Project'})
-st.write(forecast_resourcing.head())
-st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
-# st.write(pd.wide_to_long(forecast_resourcing,i='Project'))
-# st.write(forecast_resourcing.set_index('Project').unstack(level='Project').reset_index())
-# st.write(forecast_resourcing.set_index('Project').unstack())
-st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 forecast_resourcing.columns= forecast_resourcing.columns.astype(str)
-# st.write(forecast_resourcing.columns)
-# st.write(forecast_resourcing.loc[:,['Title','Total Months']])
-# st.write(forecast_resourcing.loc[:,['2021-03-01 00:00:00']])
 sliced_headcount_df=(forecast_resourcing.loc[:,'2021-03-01 00:00:00':])
 sliced_headcount=sliced_headcount_df.set_index('Project').unstack(level='Project').reset_index().rename(columns={'level_0':'date',0:'headcount'})
-st.write(sliced_headcount)
+# st.write(sliced_headcount)
+
+forecast_headcount=sliced_headcount.groupby(['Project','date'])['headcount'].sum().reset_index()
+summary_pivot= pd.pivot_table(forecast_headcount, values='headcount',index=['Project'], columns=['date'],fill_value=0)
+# st.write(forecast_headcount)
+st.write(summary_pivot)
 
 st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+st.write('this is section for actual plus forecast')
+pivot_headcount_summary=pivot_headcount_summary.drop('All',axis=1).drop(['All'])
+merged_headcount_forecast=pd.concat([pivot_headcount_summary,summary_pivot],axis=1)
+merged_headcount_forecast.loc['All']= merged_headcount_forecast.sum(numeric_only=True, axis=0)
+merged_headcount_forecast.loc[:,'All'] = merged_headcount_forecast.sum(numeric_only=True, axis=1)
+st.write(format_gp(merged_headcount_forecast.sort_values(by='All',ascending=False)))
+
+actual_plus_forecast_headcount = create_pivot_comparing_production_headcount(merged_headcount_forecast.sort_values(by='All',ascending=False)) 
+st.write(format_gp(actual_plus_forecast_headcount))
+
+
 # Project_col=forecast_resourcing.iloc[:,-1:] # to select Project
 # months_headcount=forecast_resourcing.iloc[:,25:-1] # to select from march '21 to 2nd last column as last column has Project column
 # st.write(months_headcount)
