65ae5d32139c32eb00c7a1adab5060ab3f912d19
ZeNoonan <darragh.noonan@gmail.com>
2021-03-24 21:46:37 +0000

    update

--- Ledger_historical_analysis.py
+++ Ledger_historical_analysis.py
@@ -8,7 +8,9 @@ from Ledger_helper_functions import (Budget_Raw_Clean_File,NL_Raw_Clean_File, da
 pretty_PL_format,ytd_column_forecast,month_column_forecast,pl_dept_generation, end_of_year_forecast, end_of_year_forecast_dept, gp_by_project,
 long_format_budget,long_format_nl, format_gp, gp_nl_budget_comp, budget_forecast_gp,gp_by_project_sales_cos,
 budget_forecast_gp_sales_cos, get_total_by_month,credit_notes_resolve,UK_clean_921,company_ee_project,combined_921_headcount,pivot_headcount,
-final_headcount,create_pivot_comparing_production_headcount,load_ledger_data,month_period_clean,load_data,load_16_19_clean)
+final_headcount,create_pivot_comparing_production_headcount,load_ledger_data,month_period_clean,load_data,load_16_19_clean,forecast_resourcing_function,df_concat,
+headcount_actual_plus_forecast,
+)
 
 st.set_page_config(layout="wide")
 
@@ -19,90 +21,54 @@ with st.echo():
 
 data_2020='C:/Users/Darragh/Documents/Python/Work/Data/NL_2020.xlsx'
 data_2016_19='C:/Users/Darragh/Documents/Python/Work/Data/NL_2016_2019.xlsx'
-forecast_resourcing_file='C:/Users/Darragh/Documents/Python/Work/Data/Resource_Planner_v0005_2021-03-18 11_14_58.xlsx'
+forecast_resourcing_file=('C:/Users/Darragh/Documents/Python/Work/Data/Resource_Planner_v0005_2021-03-18 11_14_58.xlsx')
+forecast_test=load_ledger_data('C:/Users/Darragh/Documents/Python/Work/Data/Resource_Planner_v0005_2021-03-18 11_14_58.xlsx')
 forecast_project_mapping=pd.read_excel('C:/Users/Darragh/Documents/Python/Work/Data/Project_Codes_2021_.xlsx', sheet_name='Sheet2')
 coding_acc_schedule = (pd.read_excel('C:/Users/Darragh/Documents/Python/Work/Data/account_numbers.xlsx')).iloc[:,:3]
 coding_sort=pd.read_excel('C:/Users/Darragh/Documents/Python/Work/Data/account_numbers.xlsx', sheet_name='Sheet2')
 Project_codes=pd.read_excel('C:/Users/Darragh/Documents/Python/Work/Data/Project_Codes_2021_.xlsx').rename(columns = {'User Code' : 'SUBANALYSIS 0'})
 
-# st.write('this is mutating below for 2021 data')
-# NL = load_ledger_data(data_2021).copy()
-# stop_mutating_df = NL_Raw_Clean_File(NL, coding_acc_schedule)
-
-# NL_Data = month_period_clean(stop_mutating_df).copy()
-
-# st.write('but looks like 2016-2019 data is not being mutatated WHY')
-# NL_16_19 = load_ledger_data(data_2016_19).copy()
-# NL_Data_16_19 = NL_Raw_Clean_File(NL_16_19, coding_acc_schedule)
-
-# NL_Data_16_19=NL_Data_16_19.join(NL_Data_16_19['Employee'].str.split(' ', expand=True).rename(columns={0:'EE',1:'Name_EE'}))
-# NL_Data_16_19['Employee - Ext. Code']=NL_Data_16_19['EE']
-# NL_Data_16_19=month_period_clean(NL_Data_16_19)
-
-# st.write('2020 data is mutating')
-# NL_20 = load_ledger_data(data_2020).copy()
-# stop_mutating_df_20=NL_Raw_Clean_File(NL_20, coding_acc_schedule)
-# NL_Data_20 = month_period_clean(stop_mutating_df_20)
-
-
 NL_Data_21=load_data(data_2021,coding_acc_schedule)
 NL_Data_20=load_data(data_2020,coding_acc_schedule)
 NL_Data_16_19=load_16_19_clean(data_2016_19,coding_acc_schedule)
 
-
-@st.cache
-def df_concat():
-    return pd.concat([NL_Data_16_19,NL_Data_20,NL_Data_21],ignore_index=True)
+# @st.cache
+# def df_concat(NL_Data_16_19,NL_Data_20,NL_Data_21):
+#     return pd.concat([NL_Data_16_19,NL_Data_20,NL_Data_21],ignore_index=True)
 # BUT I HAVE THE PROBLEM OF YEAR for example, period 3 which is equal to November in year 2016 will be made to Nov '16 when it should be Nov '15
 
+consol_headcount_data=df_concat(NL_Data_16_19,NL_Data_20,NL_Data_21).copy()
+# headcount_filtered=final_headcount(consol_headcount_data).groupby('date').head(2)
+# st.write('headcount filtered by top 2 in every month')
+# st.write(headcount_filtered)
 
-consol_headcount_data=df_concat().copy()
-headcount_filtered=final_headcount(consol_headcount_data).groupby('date').head(2)
-st.write('headcount filtered by top 2 in every month')
-st.write(headcount_filtered)
-
-pivot_headcount_summary=(pivot_headcount(final_headcount(consol_headcount_data)))
+actual_headcount_direct=(pivot_headcount(final_headcount(consol_headcount_data)))
 st.write('pivot by month of headcount')
-st.write(format_gp(pivot_headcount_summary))
+st.write(format_gp(actual_headcount_direct))
 
-st.write('data to plot headcount numbers')
-st.write(format_gp(create_pivot_comparing_production_headcount(pivot_headcount_summary)))
+# st.write('data to plot headcount numbers')
+# st.write(format_gp(create_pivot_comparing_production_headcount(pivot_headcount_summary)))
 # https://stackoverflow.com/questions/14745022/how-to-split-a-dataframe-string-column-into-two-columns
 # https://stackoverflow.com/questions/49795825/skip-nan-and-shift-elements-in-a-pandas-dataframe-row  
 
 st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 st.write('this is forecast headcount section')
-
-def forecast_resourcing(x,forecast_project_mapping,start_forecast_period_resourcing_tool):
-    x=load_ledger_data(forecast_resourcing_file).copy()
-    # forecast_project_mapping=pd.
-    x=pd.merge(x,forecast_project_mapping,on='Project',how='outer').drop('Project',axis=1).rename(columns={'Project_name':'Project'})
-    x.columns= x.columns.astype(str)
-    sliced_x=x.loc[:,start_forecast_period_resourcing_tool:]
-    sliced_x=sliced_x.set_index('Project').unstack(level='Project').reset_index().rename(columns={'level_0':'date',0:'headcount'})
-    sliced_x=sliced_x.groupby(['Project','date'])['headcount'].sum().reset_index()    
-    x = pd.pivot_table(sliced_x, values='headcount',index=['Project'], columns=['date'],fill_value=0)
-    return x
-
-forecast_resourcing=load_ledger_data(forecast_resourcing_file).copy()
-forecast_resourcing=(pd.merge(forecast_resourcing,forecast_project_mapping,on='Project',how='outer')).drop('Project',axis=1).rename(columns={'Project_name':'Project'})
-
-forecast_resourcing.columns= forecast_resourcing.columns.astype(str)
-sliced_headcount_df=(forecast_resourcing.loc[:,start_forecast_period_resourcing_tool:])
-sliced_headcount=sliced_headcount_df.set_index('Project').unstack(level='Project').reset_index().rename(columns={'level_0':'date',0:'headcount'})
-# st.write(sliced_headcount)
-
-forecast_headcount=sliced_headcount.groupby(['Project','date'])['headcount'].sum().reset_index()
-summary_pivot= pd.pivot_table(forecast_headcount, values='headcount',index=['Project'], columns=['date'],fill_value=0)
-# st.write(forecast_headcount)
-st.write(summary_pivot)
 st.write('this is a test of forecast function')
-st.write(forecast_resourcing(forecast_resourcing_file,forecast_project_mapping,start_forecast_period_resourcing_tool))
-
+forecast_headcount_direct=forecast_resourcing_function(forecast_test,forecast_project_mapping,start_forecast_period_resourcing_tool)
+st.write(forecast_headcount_direct)
 st.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+
 st.write('this is section for actual plus forecast')
-pivot_headcount_summary=pivot_headcount_summary.drop('All',axis=1).drop(['All'])
-merged_headcount_forecast=pd.concat([pivot_headcount_summary,summary_pivot],axis=1)
+
+# def headcount_actual_plus_forecast(actual_headcount_direct,forecast_headcount_direct):
+#     actual=actual_headcount_direct.drop('All',axis=1).drop(['All'])
+#     merged=pd.concat([actual,forecast_headcount_direct],axis=1)
+#     merged.loc['All']= merged.sum(numeric_only=True, axis=0)
+#     merged.loc[:,'All'] = merged_headcount_forecast.sum(numeric_only=True, axis=1)
+#     return merged.sort_values(by='All',ascending=False)
+
+pivot_headcount_summary=actual_headcount_direct.drop('All',axis=1).drop(['All'])
+merged_headcount_forecast=pd.concat([pivot_headcount_summary,forecast_headcount_direct],axis=1)
 
 st.write('Need to get the pivot data in a long format I think so that I can graph it')
 long_headcount_data = merged_headcount_forecast
@@ -113,7 +79,8 @@ st.write(long_headcount_data)
 
 merged_headcount_forecast.loc['All']= merged_headcount_forecast.sum(numeric_only=True, axis=0)
 merged_headcount_forecast.loc[:,'All'] = merged_headcount_forecast.sum(numeric_only=True, axis=1)
-st.write(format_gp(merged_headcount_forecast.sort_values(by='All',ascending=False)))
+# st.write(format_gp(merged_headcount_forecast.sort_values(by='All',ascending=False)))
+st.write('Actual plus Forecast',format_gp(headcount_actual_plus_forecast(actual_headcount_direct,forecast_headcount_direct)))
 
 actual_plus_forecast_headcount = create_pivot_comparing_production_headcount(merged_headcount_forecast.sort_values(by='All',ascending=False)) 
 st.write(format_gp(actual_plus_forecast_headcount))
