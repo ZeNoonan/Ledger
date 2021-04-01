84831b0ea13d663e800a5e6d8640495a3cb6ba9a
ZeNoonan <darragh.noonan@gmail.com>
2021-04-01 14:34:25 +0100

    update historical

--- Ledger_historical_analysis.py
+++ Ledger_historical_analysis.py
@@ -10,7 +10,8 @@ long_format_budget,long_format_nl, format_gp, gp_nl_budget_comp, budget_forecast
 budget_forecast_gp_sales_cos, get_total_by_month,credit_notes_resolve,UK_clean_921,company_ee_project,combined_921_headcount,pivot_headcount,
 final_headcount,create_pivot_comparing_production_headcount,load_ledger_data,month_period_clean,load_data,load_16_19_clean,forecast_resourcing_function,df_concat,
 headcount_actual_plus_forecast,headcount_actual_plus_forecast_with_subtotal,data_for_graphing, group_by_monthly_by_production, acc_schedule_find,
-test_gp_by_project,gp_percent_by_project,gp_revenue_concat,format_table,headcount_921_940,format_dataframe,pivot_headcount_dept,
+test_gp_by_project,gp_percent_by_project,gp_revenue_concat,format_table,headcount_921_940,format_dataframe,pivot_headcount_dept,forecast_resourcing_dept,
+test_forecast_resourcing_dept,
 )
 
 st.set_page_config(layout="wide")
@@ -87,6 +88,21 @@ with st.beta_expander('Overhead Headcount'):
     st.write(format_dataframe(pivot_headcount_dept(data_headcount_921)))
     st.write(format_dataframe(pivot_headcount_dept(data_headcount_940)))
 
+with st.beta_expander('Overall Headcount to date broken down by Department'):
+    data_graphing_actual_to_date=headcount_921_940(consol_headcount_data)
+    # st.write(data_graphing_actual_to_date.head())
+    st.write(format_dataframe(pivot_headcount_dept(data_graphing_actual_to_date)))
+
+with st.beta_expander('Overall Headcount to date broken down by Project'):
+    st.write(format_dataframe(pivot_headcount(data_graphing_actual_to_date)))
+
+with st.beta_expander('Try and work with the forecast data'):
+    # st.write(format_dataframe(pivot_headcount(data_graphing_actual_to_date)))
+    # forecast_headcount_test=forecast_resourcing_dept(forecast_test,forecast_project_mapping,start_forecast_period_resourcing_tool)
+    # st.write(format_gp(forecast_headcount_test))
+    st.write(test_forecast_resourcing_dept(forecast_test,forecast_project_mapping,start_forecast_period_resourcing_tool))
+    st.write('move the column back to end')
+    # st.write(forecast_resourcing_dept(forecast_test,forecast_project_mapping,start_forecast_period_resourcing_tool))
 
 with st.beta_expander('Historical GP Analysis'):
     # NL_Revenue = gp_by_project_sales_cos(NL_Data_21, coding_acc_schedule,Schedule_Name='Revenue', Department=None)
