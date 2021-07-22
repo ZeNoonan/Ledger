9a5bcbce656552543ef03ee54e57bbdf52bf3e3c
ZeNoonan <darragh.noonan@gmail.com>
2021-07-22 11:03:12 +0100

    update streamlit echart

--- ledger_headcount.py
+++ ledger_headcount.py
@@ -3,6 +3,7 @@ import numpy as np
 import streamlit as st
 import datetime
 import altair as alt
+from streamlit_echarts import st_echarts
 
 st.set_page_config(layout="wide")
 st.write('Select start of forecast period below and actual')
@@ -387,3 +388,181 @@ st.write('add labels to headcount would be good')
 # https://altair-viz.github.io/gallery/stacked_bar_chart_with_text.html
 # nfl chart
 
+with st.beta_expander('Trying out echarts'):
+    with open("https://github.com/andfanilo/streamlit-echarts-demo/blob/master/data/life-expectancy-table.json") as f:
+        raw_data = json.load(f)
+    countries = [
+        "Finland",
+        "France",
+        "Germany",
+        "Iceland",
+        "Norway",
+        "Poland",
+        "Russia",
+        "United Kingdom",
+    ]
+
+    datasetWithFilters = [
+        {
+            "id": f"dataset_{country}",
+            "fromDatasetId": "dataset_raw",
+            "transform": {
+                "type": "filter",
+                "config": {
+                    "and": [
+                        {"dimension": "Year", "gte": 1950},
+                        {"dimension": "Country", "=": country},
+                    ]
+                },
+            },
+        }
+        for country in countries
+    ]
+
+    seriesList = [
+        {
+            "type": "line",
+            "datasetId": f"dataset_{country}",
+            "showSymbol": False,
+            "name": country,
+            "endLabel": {
+                "show": True,
+                "formatter": JsCode(
+                    "function (params) { return params.value[3] + ': ' + params.value[0];}"
+                ).js_code,
+            },
+            "labelLayout": {"moveOverlap": "shiftY"},
+            "emphasis": {"focus": "series"},
+            "encode": {
+                "x": "Year",
+                "y": "Income",
+                "label": ["Country", "Income"],
+                "itemName": "Year",
+                "tooltip": ["Income"],
+            },
+        }
+        for country in countries
+    ]
+
+    option = {
+        "animationDuration": 10000,
+        "dataset": [{"id": "dataset_raw", "source": raw_data}] + datasetWithFilters,
+        "title": {"text": "Income in Europe since 1950"},
+        "tooltip": {"order": "valueDesc", "trigger": "axis"},
+        "xAxis": {"type": "category", "nameLocation": "middle"},
+        "yAxis": {"name": "Income"},
+        "grid": {"right": 140},
+        "series": seriesList,
+    }
+    st_echarts(options=option, height="600px")
+
+    options_1 = {
+        "title": {"text": "headcount by dept"},
+        "tooltip": {
+            "trigger": "axis",
+            "axisPointer": {"type": "cross", "label": {"backgroundColor": "#6a7985"}},
+        },
+        "legend": {"data": ["TV", "CG", "视频广告", "直接访问", "搜索引擎"]},
+        "toolbox": {"feature": {"saveAsImage": {}}},
+        "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+        "xAxis": [
+            {
+                "type": "category",
+                "boundaryGap": False,
+                "data": ["2015", "2016", "2017", "2018", "2019", "2020", "2021"],
+            }
+        ],
+        "yAxis": [{"type": "value"}],
+        "series": [
+            {
+                "name": "TV",
+                "type": "line",
+                "stack": "headcount",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": [120, 132, 101, 134, 90, 230, 210],
+            },
+            {
+                "name": "CG",
+                "type": "line",
+                "stack": "headcount",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": [220, 182, 191, 234, 290, 330, 310],
+            },
+            {
+                "name": "视频广告",
+                "type": "line",
+                "stack": "headcount",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": [150, 232, 201, 154, 190, 330, 410],
+            },
+            {
+                "name": "直接访问",
+                "type": "line",
+                "stack": "headcount",
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": [320, 332, 301, 334, 390, 330, 320],
+            },
+            {
+                "name": "搜索引擎",
+                "type": "line",
+                "stack": "headcount",
+                "label": {"show": True, "position": "top"},
+                "areaStyle": {},
+                "emphasis": {"focus": "series"},
+                "data": [820, 932, 901, 934, 1290, 1330, 1320],
+            },
+        ],
+    }
+
+    st_echarts(options=options_1, height="400px")
+
+    options_2 = {
+        "title": {"text": "折线图堆叠"},
+        "tooltip": {"trigger": "axis"},
+        "legend": {"data": ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"]},
+        "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+        "toolbox": {"feature": {"saveAsImage": {}}},
+        "xAxis": {
+            "type": "category",
+            "boundaryGap": False,
+            "data": ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+        },
+        "yAxis": {"type": "value"},
+        "series": [
+            {
+                "name": "邮件营销",
+                "type": "line",
+                "stack": "总量",
+                "data": [120, 132, 101, 134, 90, 230, 210],
+            },
+            {
+                "name": "联盟广告",
+                "type": "line",
+                "stack": "总量",
+                "data": [220, 182, 191, 234, 290, 330, 310],
+            },
+            {
+                "name": "视频广告",
+                "type": "line",
+                "stack": "总量",
+                "data": [150, 232, 201, 154, 190, 330, 410],
+            },
+            {
+                "name": "直接访问",
+                "type": "line",
+                "stack": "总量",
+                "data": [320, 332, 301, 334, 390, 330, 320],
+            },
+            {
+                "name": "搜索引擎",
+                "type": "line",
+                "stack": "总量",
+                "data": [820, 932, 901, 934, 1290, 1330, 1320],
+            },
+        ],
+    }
+    st_echarts(options=options_2, height="400px")
